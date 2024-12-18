486a714daf523fd8194606e874a01dbd59298170
carlosfrontend <11649745+carlosfrontend@users.noreply.github.com>
2024-12-18 17:58:43 +0100

    Feat: Extract to components Tic tact toe

--- proyectos/01-tic-tact-toe/src/App.jsx
+++ proyectos/01-tic-tact-toe/src/App.jsx
@@ -1,8 +1,68 @@
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import confetti from "canvas-confetti";
+import { Square } from "./components/Square";
+import { TURNS } from "./constants";
+import { checkWinner, checkEndGame } from "./logic/board";
+import { WinnerModal } from "./components/WinnerModal";
 
 function App() {
+  const [board, setBoard] = useState(Array(9).fill(null));
+  const [turn, setTurn] = useState(TURNS.X);
+  // null = no hay ganador, false = no hay empate
+  const [winner, setWinner] = useState(null);
+  
+  const resetGame = () => {
+    setBoard(Array(9).fill(null));
+    setTurn(TURNS.X);
+    setWinner(null);
+  };
 
-  return <h1>Tic tac toe</h1>
+  const updateBoard = (index) => {
+    // No actualizamos esta posición
+    //si ya tiene algo
+    if (board[index] !== null || winner) return;
+    // Actualizamos el tablero
+    const newBoard = structuredClone(board);
+    newBoard[index] = turn;
+    setBoard(newBoard);
+    // Cambiamos el turno
+    const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X;
+    setTurn(newTurn);
+    // Comprobamos si hay ganador
+    const newWinner = checkWinner(newBoard);
+    if (newWinner) {
+      confetti();
+      setWinner(newWinner);
+    } else if (checkEndGame(newBoard)) {
+      setWinner(false)
+    }
+  };
+
+  return (
+    <main className="board">
+      <h1>Tic tac toe</h1>
+      <button onClick={resetGame}>Reset del juego</button>
+      <section className="game">
+        {board.map((square, index) => {
+          return (
+            <Square
+              key={index}
+              index={index}
+              updateBoard={() => updateBoard(index)}
+            >
+              {square}
+            </Square>
+          );
+        })}
+      </section>
+      <section className="turn">
+        <Square isSelected={turn === TURNS.X}>{TURNS.X}</Square>
+        <Square isSelected={turn === TURNS.O}>{TURNS.O}</Square>
+      </section>
+     <WinnerModal winner={winner} resetGame={resetGame} />
+    </main>
+  );
 }
 
-export default App
+export default App;
---
+++ proyectos/01-tic-tact-toe/src/components/WinnerModal.jsx
@@ -0,0 +1,27 @@
+import { Square } from "./Square";
+import { PropTypes } from "prop-types";
+
+export const WinnerModal = ({ winner, resetGame }) => {
+  const winnerText = winner === false ? "Empate" : "Ganó: ";
+  if (winner === null) return null;
+  return (
+    <section className="winner">
+      <div className="text">
+        <h2>{winnerText}</h2>
+        <header className="win">{winner && <Square>{winner}</Square>}</header>
+        <footer>
+          <button onClick={resetGame}>Empezar de nuevo</button>
+        </footer>
+      </div>
+    </section>
+  );
+};
+
+WinnerModal.propTypes = {
+  winner: PropTypes.oneOfType([
+    PropTypes.string,
+    PropTypes.bool,
+    PropTypes.oneOf([null]),
+  ]),
+  resetGame: PropTypes.func,
+};
